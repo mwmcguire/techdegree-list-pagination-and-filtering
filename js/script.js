4247a4979ff11c29044899c45643e825b2b93893
“Michael_McGuire“ <49034356+mwmcguire@users.noreply.github.com>
2020-04-22 18:56:04 -0400

    revised comments. app functioning.  need to remove the active class name from all pagination links when pagination link is clicked

--- js/script.js
+++ js/script.js
@@ -3,50 +3,25 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
 
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
+// global variables
 let listItem = document.getElementsByClassName('student-item cf');
 let itemsPerPage = 10;
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
+// function to display list items to the page
 const showPage = (list, page) => {
   let startIndex = page * itemsPerPage - itemsPerPage;
   let endIndex = page * itemsPerPage;
 
   for (let i = 0; i < list.length; i++) {
     if ((i >= startIndex) & (i < endIndex)) {
-      console.log(list[i]);
+      list[i].style.display = '';
+    } else {
+      list[i].style.display = 'none';
     }
   }
 };
 
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
+// function to append link items to the page
 const appendPageLinks = (list) => {
   const page = document.querySelector('.page');
   const div = document.createElement('div');
@@ -57,7 +32,7 @@ const appendPageLinks = (list) => {
   div.appendChild(ul);
 
   let numOfPages = list.length / itemsPerPage;
-  for (let i = 0; i < numOfPages; i++) {
+  for (let i = 1; i < numOfPages; i++) {
     const li = document.createElement('li');
     const a = document.createElement('a');
 
@@ -65,7 +40,17 @@ const appendPageLinks = (list) => {
     li.appendChild(a);
     a.href = '#';
     a.textContent = i;
+
+    a.addEventListener('click', (e) => {
+      for (let i = 0; i < numOfPages.length; i++) {
+        a.className = '';
+      }
+      e.target.className = 'active';
+      showPage(listItem, a.textContent);
+    });
   }
 };
 
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+// function calls
+showPage(listItem, 1);
+appendPageLinks(listItem);
