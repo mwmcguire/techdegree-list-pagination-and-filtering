094fe74a1b9f9037e40e3bd14d0ff045b53aecb3
“Michael_McGuire“ <49034356+mwmcguire@users.noreply.github.com>
2020-04-23 16:33:54 -0400

    search bar created. search functionality incomplete

--- js/script.js
+++ js/script.js
@@ -5,7 +5,52 @@ FSJS project 2 - List Filter and Pagination
 
 // global variables
 let listItems = document.getElementsByClassName('student-item cf');
+let searchResults = [];
 const itemsPerPage = 10;
+const page = document.querySelector('.page');
+
+// dynamic search bar
+const searchBar = document.createElement('input');
+searchBar.setAttribute('placeholder', 'Search for students...');
+
+const searchBarDiv = document.createElement('div');
+searchBarDiv.className = 'student-search';
+
+const submitBtn = document.createElement('button');
+submitBtn.textContent = 'Search';
+
+const pageHeader = document.getElementsByClassName('page-header cf');
+
+pageHeader[0].appendChild(searchBarDiv);
+searchBarDiv.appendChild(searchBar);
+searchBarDiv.appendChild(submitBtn);
+
+const performSearch = (input, names) => {
+  for (let i = 0; i < names.length; i++) {
+    let name = names[i].textContent.toLowerCase();
+    let inputValue = input.value.toLowerCase();
+
+    if (inputValue.length !== 0 && name.includes(inputValue)) {
+      console.log(names[i]);
+      searchResults.push(names[i]);
+    }
+  }
+};
+
+submitBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  performSearch(searchBar, listItems);
+  showPage(searchResults, 1);
+
+  const pageLinks = document.querySelector('.pagination');
+  page.removeChild(pageLinks);
+  console.log('submit button is functional');
+});
+
+searchBar.addEventListener('keyup', () => {
+  performSearch(searchBar, listItems);
+  console.log('keyup event on search input is functional');
+});
 
 // function to display list items to the page
 const showPage = (list, page) => {
@@ -23,7 +68,6 @@ const showPage = (list, page) => {
 
 // function to append link items to the page
 const appendPageLinks = (list) => {
-  const page = document.querySelector('.page');
   const div = document.createElement('div');
   const ul = document.createElement('ul');
 
@@ -43,9 +87,10 @@ const appendPageLinks = (list) => {
     a.textContent = i;
 
     a.addEventListener('click', (e) => {
+      e.preventDefault();
       const activeClass = document.querySelector('.active');
       if (activeClass) {
-        activeClass.className = '';
+        activeClass.classList.remove('active');
       }
       e.target.className = 'active';
       showPage(listItems, a.textContent);
